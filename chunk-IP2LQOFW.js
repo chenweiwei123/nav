4aefdfc0be368e69a48d5ecb8f23640b238f510e
chenweiwei123 <38514296+chenweiwei123@users.noreply.github.com>
2025-08-15 05:54:56 +0000

    Deploying to gh-pages from @ chenweiwei123/nav@2f30ef834c08fc415b243665068e83df8a7ebb67 🚀

---
+++ chunk-IP2LQOFW.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-6TLLKO7N.js";import{pb as r,yb as e}from"./chunk-CJEKEF6O.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
